1d3571403e5b4b0a8a30ae2a39bf84dce72c5d02
Saurabh Sharma <83974389+saurabh549@users.noreply.github.com>
2021-05-10 23:33:20 +0530

    Add files via upload

---
+++ assets/js/app.js
@@ -0,0 +1,25 @@
+//listen of a submit
+document.querySelector(".contact-form").addEventListener("submit",submitForm);
+function submitForm(e) {
+    e.preventDefault();
+    //get input values
+    let fullname=document.querySelector(".fullname").value;
+    let email=document.querySelector(".email").value;
+    let message=document.querySelector(".message").value;
+
+    document.querySelector(".contact-form").reset();
+
+    sendEmail(fullname,email,message);
+}
+//send email info
+function sendEmail(fullname,email,message) {
+    Email.send({
+        Host:"smtp.gmail.com",
+        Username:'saurabh5692792@gmail.com',
+        Password:"tapcwpyaylrdkxui",
+        To:'saurabh5692792@gmail.com',
+        From:'saurabh5692792@gmail.com',
+        Subject:`${fullname} sent you a message`,
+        Body:`Name: ${fullname} <br/> Email: ${email} <br/> Message: ${message}`,
+    }).then((message) => alert("mail sent successfully"));
+}
